a7638b24aaf0d4bd1e0d03718c60038e233b8271
OD1N <volodin12340987@gmail.com>
2026-05-14 20:41:44 +0300

    CHECKSTYLE LALALLALLA IM CRAZY

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -6,7 +6,10 @@ import ru.yandex.practicum.filmorate.storage.Storage;
 import java.util.List;
 
 public interface FilmStorage extends Storage<Film> {
+
     void addLike(int filmId, int userId);
+
     void removeLike(int filmId, int userId);
+
     List<Film> getPopular(int count);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -6,8 +6,12 @@ import ru.yandex.practicum.filmorate.storage.Storage;
 import java.util.List;
 
 public interface UserStorage extends Storage<User> {
+
     void addFriend(int userId, int friendId);
+
     void removeFriend(int userId, int friendId);
+
     List<User> getFriends(int userId);
+
     List<User> getCommonFriends(int userId, int otherId);
 }
