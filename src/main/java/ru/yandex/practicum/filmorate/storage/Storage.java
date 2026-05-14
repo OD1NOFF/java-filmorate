46285563da43bfc53ec4072393d2a75d039254ce
OD1N <volodin12340987@gmail.com>
2026-05-14 20:38:10 +0300

    CHECKSTYLE UHIDJVIUA(UVGBI(DHBNVNADVNH_POAD)PVIJ{_DVK

--- src/main/java/ru/yandex/practicum/filmorate/storage/Storage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/Storage.java
@@ -4,8 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 public interface Storage<T> {
+
     T add(T item);
+
     T update(T item);
+
     List<T> getAll();
+
     Optional<T> getById(int id);
 }
